a6d1a39f0e461e6188db2ef95eb9f52a201dff9a
chowon kim <ch1dnjs2@naver.com>
2022-07-12 00:27:23 +0900

    init Calender

---
+++ src/project01.java
@@ -0,0 +1,17 @@
+/* 주석 작성해보기 */
+
+/* 
+ Date : 2022. 07. 11
+ Source : Hello. java
+ Author : 김초원
+ Email : ch1dnjs2@naver.com 
+*/
+
+class Hello 
+{
+		public static void main(String[] args) /* 프로그램의 시작 */
+		{
+				System.out.println("Hello, World"); // Hello, World를 출력
+		}
+}
+
